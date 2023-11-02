f3a44f5e222fb66ea747e17f2a342c76765fa2ee
Taka011 <130344178+Taka011@users.noreply.github.com>
2023-11-02 11:17:41 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,11 +18,23 @@ $(function () {
         $("body,html").animate({ scrollTop: 0, }, 300);
         return false;
     });
-
-    // ページフェードイン(ロードアニメーション)
-    $(window).on('load', function() {
-        setTimeout(function(){
-            $('.mask').fadeOut(500);
-        },1000);
-    })
+        
+    // ウインドウをスクロールしたら
+    $(window).scroll(function (){
+        // .js-fadeのクラスを持つ要素（それぞれに対して）
+        $('.js-fade').each(function(){
+            // この要素のスクロール位置（上から）
+            var pos = $(this).offset().top;
+            // ウインドウのスクロール量（上から）
+            var scroll = $(window).scrollTop();
+            // ウインドウの縦幅
+            var windowHeight = $(window).height();
+            // ウインドウのスクロール量（上から）が
+            // この要素のスクロール位置 - ウインドウの縦幅 + 100pxより大きい場合
+            if (scroll > pos - windowHeight + 100){
+                // .scrollというクラス.js-fadeに付与する
+                $(this).addClass('scroll');
+            }
+        });
+    });
 });
