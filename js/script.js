57065aac5938c5276c51ecb0ebd96bd1cb582779
Taka011 <130344178+Taka011@users.noreply.github.com>
2023-11-03 16:26:23 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,20 +19,13 @@ $(function () {
         return false;
     });
         
-    // ウインドウをスクロールしたら
+    // ウインドウスクロールイベント
     $(window).scroll(function (){
-        // .js-fadeのクラスを持つ要素（それぞれに対して）
         $('.js-fade').each(function(){
-            // この要素のスクロール位置（上から）
-            var pos = $(this).offset().top;
-            // ウインドウのスクロール量（上から）
-            var scroll = $(window).scrollTop();
-            // ウインドウの縦幅
-            var windowHeight = $(window).height();
-            // ウインドウのスクロール量（上から）が
-            // この要素のスクロール位置 - ウインドウの縦幅 + 100pxより大きい場合
+            let pos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
             if (scroll > pos - windowHeight + 100){
-                // .scrollというクラス.js-fadeに付与する
                 $(this).addClass('scroll');
             }
         });
